1a91c78a2f5b3de91604d7b2de891ce9256ee037
socrates77 <zhwenrong@sina.com>
2016-10-09 21:48:25 +0800

    add ch02/more.py
    Signed-off-by: socrates77 <zhwenrong@sina.com>

---
+++ ch02/more.py
@@ -0,0 +1,22 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'socrates'
+
+
+def more(text, numlines=15):
+    lines = text.splitlines()
+    while lines:
+        chunk = lines[:numlines]
+        lines = lines[numlines:]
+        for line in chunk:
+            print(line)
+        if lines and input('More?') not in ['y', 'Y']:
+            break
+
+
+if __name__ == '__main__':
+    import sys
+
+    print(sys.argv[1])
+    more(open(sys.argv[1]).read(), 10)
---
+++ ch02/try.py
@@ -0,0 +1,9 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'socrates'
+
+import os
+import sys
+
+print(os.__doc__)
