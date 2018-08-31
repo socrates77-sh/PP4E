a070d5f18410dcd4675f7f8af089d7783b02d5c8
socrates77 <zhwenrong@sina.com>
2018-08-31 17:58:44 +0800

    learn ch10 @office
    Signed-off-by: socrates77 <zhwenrong@sina.com>

---
+++ ch10/rad.py
@@ -0,0 +1,32 @@
+# reload callback handlers dynamically
+
+from tkinter import *
+import radactions           # get initial callback handlers
+from imp import reload      # moved to a module in Python 3.X
+
+
+class Hello(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.make_widgets()
+
+    def make_widgets(self):
+        Button(self, text='message1', command=self.message1).pack(side=LEFT)
+        Button(self, text='message2', command=self.message2).pack(side=RIGHT)
+
+    def message1(self):
+        # need to reload actions module before calling
+        reload(radactions)
+        radactions.message1()      # now new version triggered by pressing button
+
+    def message2(self):
+        # changes to radactions.py picked up by reload
+        reload(radactions)
+        radactions.message2(self)  # call the most recent version; pass self
+
+    def method1(self):
+        print('exposed method...')       # called from radactions function
+
+Hello().mainloop()
